a338272468fa02eed2a54dddb7fdd3dc1be0d141
its-Pradyot <itkurkarpradyot@gmail.com>
2021-01-05 15:09:46 +0530

    3 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit, VERSION } from "@angular/core";
+import { DemoService } from "./demo.service";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent implements OnInit {
+  name = "Angular " + VERSION.major;
+
+  arr;
+  constructor(private service: DemoService) {}
+  ngOnInit(): void {
+    this.service.getServerData().subscribe(data => {
+      this.arr = Array.from(Object.keys(data), k => data[k]);
+    });
+  }
 }
